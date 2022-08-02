e063478d31fd21c90dce7861c0278c0c8d9ba130
Quinn R Davis <quinn.r.davis@gmail.com>
2022-08-02 10:56:32 -0700

    added comments

--- more-projects/abstract_classes.py
+++ more-projects/abstract_classes.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+# parent class with a regular method and an abstract method
 class Parent(ABC):
     def regularMethod(self, num):
         print("Your number is {}".format(num))
@@ -8,6 +9,7 @@ class Parent(ABC):
     def abstractMethod(self, num):
         pass
 
+# child class that extends the parent and defines the abstract method
 class Child(Parent):
     def abstractMethod(self, num):
         print("Your number is {}, this is an abstract method".format(num))
